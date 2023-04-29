c8416016e5c5bf38bd8536e6183bc6d09b904b70
Farheen khan <farheenkhancc@gmail.com>
2023-04-29 15:54:04 +0530

    Setting up the CI/CD workflow for building image at docker

--- backend/src/main/java/com/webapp/application/videostreamingwebapp/dto/VideoDto.java
+++ backend/src/main/java/com/webapp/application/videostreamingwebapp/dto/VideoDto.java
@@ -1,1 +1,23 @@
-package com.webapp.application.videostreamingwebapp.dto;import com.webapp.application.videostreamingwebapp.model.Comment;import com.webapp.application.videostreamingwebapp.model.VideoStatus;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;import java.util.Set;@Data@NoArgsConstructor@AllArgsConstructorpublic class VideoDto {    private String id;    private String title;    private String description;    private Set<String> tags;    private String videoUrl;    private VideoStatus videoStatus;    private String thumbnailUrl;}
+package com.webapp.application.videostreamingwebapp.dto;
+
+import com.webapp.application.videostreamingwebapp.model.Comment;
+import com.webapp.application.videostreamingwebapp.model.VideoStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoDto {
+    private String id;
+    private String title;
+    private String description;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private String thumbnailUrl;
+}
