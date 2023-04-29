9c7b3e6fa1e28424e158ed784fd29e08b0901911
Farheen khan <farheenkhancc@gmail.com>
2023-04-29 14:58:36 +0530

    Wrote the documetation for Audience , SecurityConfig, VideoDTO , an save-video-details , upload-video

--- backend/src/main/java/com/webapp/application/videostreamingwebapp/config/SecurityConfig.java
+++ backend/src/main/java/com/webapp/application/videostreamingwebapp/config/SecurityConfig.java
@@ -1,1 +1,60 @@
-package com.webapp.application.videostreamingwebapp.config;import org.springframework.beans.factory.annotation.Value;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.security.config.Customizer;import org.springframework.security.config.annotation.web.builders.HttpSecurity;import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;import org.springframework.security.config.http.SessionCreationPolicy;import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;import org.springframework.security.oauth2.core.OAuth2TokenValidator;import org.springframework.security.oauth2.jwt.*;import org.springframework.security.web.SecurityFilterChain;@Configuration@EnableWebSecuritypublic class SecurityConfig{    @Value("${spring.security.oauth2.resourceserver.jwt-issuer-uri}")    private String issuer;    @Value("${auth0.audience}")    private String audience;      @Bean      public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {          httpSecurity.authorizeHttpRequests()                  .anyRequest().authenticated()                  .and()                  .sessionManagement()                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS)                  .and()                  .cors(Customizer.withDefaults())                  .oauth2ResourceServer()                  .jwt();          return httpSecurity.build();      }    @Bean    JwtDecoder jwtDecoder() {        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)                JwtDecoders.fromOidcIssuerLocation(issuer);        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);        jwtDecoder.setJwtValidator(withAudience);        return jwtDecoder;    }}
+package com.webapp.application.videostreamingwebapp.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Value("${spring.security.oauth2.resourceserver.jwt-issuer-uri}")
+    private String issuer;
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+      @Bean
+      public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+          httpSecurity.authorizeHttpRequests()
+                  .anyRequest().authenticated()
+                  .and()
+                  .sessionManagement()
+                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                  .and()
+                  .cors(Customizer.withDefaults())
+                  .oauth2ResourceServer()
+                  .jwt();
+
+          return httpSecurity.build();
+      }
+
+    /**
+     * This function creates a JWT decoder with validators for audience and issuer.
+     * 
+     * @return A bean of type `JwtDecoder` is being returned.
+     */
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+}
